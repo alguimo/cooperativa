82da2b63690dbad4905c29d9bae1dfe032a3c1fa
Álvaro Guiñón <alvaro@mac-mini-de-alvaro.home>
2026-07-02 16:31:40 +0200

    1

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 let transport = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
+    family: 4,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
